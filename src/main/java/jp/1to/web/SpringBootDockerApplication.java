ced648a72a38033e398e06e40a827c66ed26a00f
yujiroito <yujiro@1to.jp>
2024-07-04 18:32:35 +0900

    コメント

---
+++ src/main/java/jp/1to/web/SpringBootDockerApplication.java
@@ -0,0 +1,13 @@
+package jp._1to.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/jp/1to/web/controller/HelloController.java
@@ -0,0 +1,16 @@
+package jp._1to.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    public String getIndex() {
+        return "ReversiblePiece";
+    }
+}
